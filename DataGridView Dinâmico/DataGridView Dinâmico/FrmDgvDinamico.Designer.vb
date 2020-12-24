00df255c96bb1065e23a458038525c0c0f3ed0ea
Luiz Eduardo Santafosta <46579850+Santafosta1999@users.noreply.github.com>
2020-12-24 12:04:25 -0300

    Add files via upload

---
+++ DataGridView Dinâmico/DataGridView Dinâmico/FrmDgvDinamico.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmDgvDinamico
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DgvDados = New System.Windows.Forms.DataGridView()
+        Me.BtnIncluir = New System.Windows.Forms.Button()
+        Me.BtnExcluir = New System.Windows.Forms.Button()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TxtVlrTotal = New System.Windows.Forms.TextBox()
+        CType(Me.DgvDados, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DgvDados
+        '
+        Me.DgvDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DgvDados.Location = New System.Drawing.Point(18, 18)
+        Me.DgvDados.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.DgvDados.Name = "DgvDados"
+        Me.DgvDados.Size = New System.Drawing.Size(1000, 461)
+        Me.DgvDados.TabIndex = 0
+        '
+        'BtnIncluir
+        '
+        Me.BtnIncluir.Location = New System.Drawing.Point(18, 542)
+        Me.BtnIncluir.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.BtnIncluir.Name = "BtnIncluir"
+        Me.BtnIncluir.Size = New System.Drawing.Size(222, 55)
+        Me.BtnIncluir.TabIndex = 1
+        Me.BtnIncluir.Text = "Incluir Linha - F7"
+        Me.BtnIncluir.UseVisualStyleBackColor = True
+        '
+        'BtnExcluir
+        '
+        Me.BtnExcluir.Location = New System.Drawing.Point(302, 542)
+        Me.BtnExcluir.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.BtnExcluir.Name = "BtnExcluir"
+        Me.BtnExcluir.Size = New System.Drawing.Size(222, 55)
+        Me.BtnExcluir.TabIndex = 2
+        Me.BtnExcluir.Text = "Excluir Linha - F8"
+        Me.BtnExcluir.UseVisualStyleBackColor = True
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(15, 484)
+        Me.LinkLabel1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(225, 20)
+        Me.LinkLabel1.TabIndex = 3
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "Aprenda mais sobre o VB.NET"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(785, 559)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(89, 20)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Valor Total:"
+        '
+        'TxtVlrTotal
+        '
+        Me.TxtVlrTotal.Location = New System.Drawing.Point(882, 556)
+        Me.TxtVlrTotal.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.TxtVlrTotal.Name = "TxtVlrTotal"
+        Me.TxtVlrTotal.Size = New System.Drawing.Size(136, 26)
+        Me.TxtVlrTotal.TabIndex = 5
+        '
+        'FrmDgvDinamico
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1031, 611)
+        Me.Controls.Add(Me.TxtVlrTotal)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.BtnExcluir)
+        Me.Controls.Add(Me.BtnIncluir)
+        Me.Controls.Add(Me.DgvDados)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "FrmDgvDinamico"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "DataGridView Dinâmico"
+        CType(Me.DgvDados, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DgvDados As DataGridView
+    Friend WithEvents BtnIncluir As Button
+    Friend WithEvents BtnExcluir As Button
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TxtVlrTotal As TextBox
+End Class
---
+++ DataGridView Dinâmico/DataGridView Dinâmico/FrmDgvDinamico.vb
@@ -0,0 +1,156 @@
+﻿Public Class FrmDgvDinamico
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+        Call InicializaGrid()
+
+    End Sub
+
+    Private Sub InicializaGrid()
+        With DgvDados
+            .RowHeadersVisible = False
+            .AllowUserToAddRows = False
+            .AllowUserToDeleteRows = False
+            .AllowUserToResizeColumns = False
+            .MultiSelect = False
+            .ReadOnly = False
+            '
+            'Colunas
+            '
+            .Columns.Add("PK", "Chave Primária")
+            .Columns.Add("ProdutoID", "Produto ID")
+            .Columns.Add("ProdutoNome", "Nome do Produto")
+            .Columns.Add("Quantidade", "Quantidade")
+            .Columns("Quantidade").ValueType = GetType(Integer)
+            .Columns.Add("Preco", "Preço Unitário")
+            .Columns("Preco").ValueType = GetType(Double)
+            .Columns.Add("Total", "Total")
+            .Columns("Total").ValueType = GetType(Double)
+            '
+            'Auto resize em todas as colunas
+            '
+            .AutoSizeColumnsMode = DataGridViewAutoSizeColumnMode.Fill
+            .AutoResizeColumns()
+            .Font = New Font(Me.Font, 12)
+            '
+            'Coluna Total - Somente Leitura
+            '
+            With .Columns("Total")
+                .ReadOnly = True
+                .DefaultCellStyle.BackColor = System.Drawing.Color.LightGoldenrodYellow
+                .DefaultCellStyle.ForeColor = System.Drawing.Color.Red
+                .DefaultCellStyle.Font = New Font(Me.Font, FontStyle.Bold)
+            End With
+            '
+            'Estilo do Cabeçalho
+            '
+            With .ColumnHeadersDefaultCellStyle
+                .BackColor = Color.Navy
+                .ForeColor = Color.White
+                .Font = New Font(Me.Font, 12)
+                .Font = New Font(Me.Font, FontStyle.Bold)
+            End With
+            '
+            'Alinha somente as colinas 3 a 5
+            '
+            For i As Byte = 3 To 5
+                'Alinha Cabeçalho
+                .Columns(i).HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleRight
+                'Alinha Celula
+                .Columns(i).DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight
+            Next
+        End With
+    End Sub
+
+    Private Sub FrmDgvDinamico_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.CenterToScreen()
+        '
+        'Define a caixa de texto somente leitura e limpa o conteúdo
+        '
+        TxtVlrTotal.ReadOnly = True
+        TxtVlrTotal.Clear()
+    End Sub
+
+    Private Sub BtnIncluir_Click(sender As Object, e As EventArgs) Handles BtnIncluir.Click
+        Dim Position As Integer = DgvDados.Rows.Count - 1
+        Dim PK As Integer = 1
+        '
+        'Pega o valor da última linha
+        '
+        Dim UltimaLinha As New DataGridViewRow
+        '
+        'Tem linhas?
+        '
+        If Position >= 0 Then
+            '
+            'Pega as linhas do DataGridView
+            '
+            UltimaLinha = DgvDados.Rows.OfType(Of DataGridViewRow).Last()
+            '
+            'Incrementa a PK (Column = 0)
+            '
+            PK = UltimaLinha.Cells(0).Value + 1
+        End If
+        '
+        'Dados de Exemplo: Chave, Código do Produto, Nome do Produto, Quantidade e Valor
+        '
+        Dim linha As String() = New String() {PK, "PROD00" & PK, "Produto " & PK, 1, 0.0, 0.0}
+        '
+        'Adiciona a Linha no Grid
+        '
+        DgvDados.Rows.Add(linha)
+        '
+        'Foco na celula da coluna(3) ou quantidade
+        '
+        DgvDados.CurrentCell = DgvDados.Rows(DgvDados.RowCount - 1).Cells(3)
+        DgvDados.Focus()
+        '
+        'Calcula o total
+        '
+        Call CalculaSomaTotal()
+    End Sub
+
+    Private Sub BtnExcluir_Click(sender As Object, e As EventArgs) Handles BtnExcluir.Click
+        '
+        'Se não tem linha selecionada então sai da Sub
+        '
+        If DgvDados.RowCount = 0 Then Exit Sub
+        '
+        'Remove a linha atual selecionada
+        '
+        DgvDados.Rows.Remove(DgvDados.CurrentRow)
+        DgvDados.Refresh()
+        '
+        'Se existem linhas calcula a soma
+        '
+        If DgvDados.RowCount > 0 Then Call CalculaSomaTotal()
+
+    End Sub
+
+    '
+    'Calcula a soma total (Coluna Index = 5)
+    '
+    Private Sub CalculaSomaTotal()
+        '
+        'Define o valor igual a zero
+        '
+        TxtVlrTotal.Text = "0.00"
+        '
+        'Percorre as linhas do grid
+        '
+        For i As Byte = 0 To DgvDados.RowCount - 1
+            '
+            'Converte a célula da coluna 5 para Double (usa CDbl ao estilo VB6)
+            '
+            TxtVlrTotal.Text = CDbl(TxtVlrTotal.Text) + CDbl(DgvDados.Rows(i).Cells(5).Value)
+        Next
+        '
+        'Formata e exibe o valor da soma
+        '
+        TxtVlrTotal.Text = Format(CDbl(TxtVlrTotal.Text), "#,##0.00")
+    End Sub
+End Class
